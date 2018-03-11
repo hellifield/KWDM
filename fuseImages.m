a57b94d44bf84c80c20d625d7f5a5f73cce84149
unknown <elsnermartyna@gmail.com>
2018-03-11 22:39:13 +0100

    test test test

--- fuseImages.m
+++
@@ -1,6 +0,0 @@
-function [ fusionImage ] = fuseImages( irImage, visImage, tform )
-    rOriginal = imref2d(size(irImage));
-    recovered = imwarp(visImage,tform,'OutputView',rOriginal);
-    fusionImage = imfuse(irImage, recovered);
-end
-
